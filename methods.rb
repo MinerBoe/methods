54c332a8d7621245877f64c17b3137c163689e96
Mats Dale <14minerboe16@gmail.com>
2017-09-22 17:02:08 -0600

    Implement close_far

--- methods.rb
+++ methods.rb
@@ -11,10 +11,10 @@
 
 # sleep_in?
 #
-# Students sleep in if it is not a weekday or we're on vacation. 
+# Students sleep in if it is not a weekday or we're on vacation.
 #
 # > Parameters
-# ------------ 
+# ------------
 #   * weekday  - true only if it is a weekday
 #   * vacation - true only if the student is on vacation
 
@@ -28,12 +28,12 @@ end
 
 # monkey_trouble?
 #
-# We have two monkeys, a and b, and the parameters a_smile and b_smile 
-# indicate if each is smiling. 
+# We have two monkeys, a and b, and the parameters a_smile and b_smile
+# indicate if each is smiling.
 # We are in trouble if they are both smiling or if neither of them is smiling.
 # Return true if we are in trouble.
 #
-# > Parameters 
+# > Parameters
 # ------------
 #   * a_smiling   - true only if monkey a is smiling
 #   * b_smiling   - true only if monkey b is smiling
@@ -50,9 +50,9 @@ end
 # sum_double
 #
 # Given two int values, return their sum. Unless the two values are the same,
-# then return double their sum. 
+# then return double their sum.
 #
-# > Parameters 
+# > Parameters
 # ------------
 #   * a - an integer
 #   * b - an integer
@@ -80,10 +80,10 @@ end
 
 # blackjack
 #
-# Given 2 int values greater than 0, return whichever value is nearest to 21 
+# Given 2 int values greater than 0, return whichever value is nearest to 21
 # without going over. Return 0 if they both go over.
 #
-# > Parameters 
+# > Parameters
 # ------------
 #   * a - an integer
 #   * b - an integer
@@ -99,32 +99,32 @@ def blackjack(a, b)
 	if dist_a < 0
 		return b
 	end
-	
+
 	if dist_b < 0
-		return a 
+		return a
 	end
 
 	if dist_a > dist_b
 		return b
 	end
-	
+
 	return a
 end
 
 # n_twice
 #
-# Given a string and an int n, return a string made of the first and last n 
+# Given a string and an int n, return a string made of the first and last n
 # chars from the string.
 #
 # Hint | Use string.slice
 #      | http://ruby-doc.org/core-2.2.0/String.html#method-i-slice
 #
-# > Parameters 
+# > Parameters
 # ------------
 #   * str - a string with length at least n
 #   * n   - an integer
-#
-# TODO - write n_twice (tests first)
+
+
 def n_twice(str, n)
 	if n > str.length
 		return str
@@ -134,14 +134,31 @@ end
 
 # close_far
 #
-# Given three ints, a b c, return true if one of b or c is "close" (differing 
-# from a by at most 1), while the other is "far", differing from both other 
+# Given three ints, a b c, return true if one of b or c is "close" (differing
+# from a by at most 1), while the other is "far", differing from both other
 # values by 2 or more. Note: n.abs computes the absolute value of number n.
 #
-# > Parameters 
+# > Parameters
 # ------------
 #   * a - an integer
 #   * b - an integer
 #   * c - an integer
 #
 # TODO - write close_far (tests first)
+
+def close_far(a, b, c)
+	dist_ab = (b-a).abs
+	dist_ac = (c-a).abs
+	is_close = false
+	is_far = false
+	if dist_ab <= 1 || dist_ac <= 1
+		is_close = true
+	end
+	if dist_ab >= 2 || dist_ac >= 2
+		is_far = true
+	end
+	if is_close && is_far
+		return true
+	end
+	return false
+end
